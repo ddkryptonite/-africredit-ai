751e7d22f46a55b92b13a860a2948c38fcda0015
Daniel Dogbey <ddkryptonite@gmail.com>
2025-03-14 20:42:24 +0000

    Update test.py

--- test.py
+++ test.py
@@ -14,7 +14,8 @@ DB_PORT = os.getenv("DB_PORT", "5439")
 try:
     connection_url = f"postgresql+psycopg2://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode=require"
     
-    engine = create_engine(connection_url, connect_args={"options": "-c standard_conforming_strings=off"})
+    engine = create_engine(connection_url, connect_args={"options": "-c search_path=public"})
+
 
     with engine.connect() as conn:
         result = conn.execute("SELECT 1")
