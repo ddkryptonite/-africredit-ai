6cb1685e2405f0dc93b3befd6d16aa5879407c7f
Daniel Dogbey <ddkryptonite@gmail.com>
2025-03-27 11:17:07 +0000

    Update test.py

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ DB_USERNAME = os.getenv("DB_USERNAME")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_NAME = os.getenv("DB_NAME")
-DB_PORT = os.getenv("DB_PORT", "5439")
+DB_PORT = os.getenv("DB_PORT")
 
 # Initialize database connection
 try:
